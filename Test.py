f05a673e57751c28e8db7e466bf36764b660ab9d
Shashi Jangra <shashijangra@live.com>
2024-05-27 22:30:16 +0530

    update scripts

--- Test.py
+++ Test.py
@@ -1,47 +1,50 @@
 from bs4 import BeautifulSoup as bs
-import urllib.request
+import requests
 import argparse
-import sys
 import os
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-u","--url",help="Enter URL to the problem")
-ap.add_argument("-f","--file",help="Enter path of file to be tested",required=True)
+ap.add_argument(
+    "-f", "--file", help="Enter path of file to be tested", required=True)
 args = vars(ap.parse_args())
 
 def getUrlFromFile():
-    f = open(args["file"],"r")
+    f = open(args["file"], "r")
     URL = f.read().split('\n')[0]
     f.close()
-    pos = URL.find("https")
-    if pos!=-1:
+    pos = URL.find("http")
+    if pos != -1:
         return URL[pos:]
     return None
 
-args["url"] = args["url"] if args["url"] else getUrlFromFile()
+
+args["url"] = getUrlFromFile()
+
 
 def getTests():
-    inputs,outputs = [],[]
-    source = urllib.request.urlopen(args["url"]).read()
-    soup = bs(source,"lxml")
+    inputs, outputs = [], []
+    res = requests.get(args["url"]).text
+    soup = bs(res, 'html.parser')
 
-    def scrapeText(div):
-        temp = str(div.find('pre'))
-        temp = temp.replace("<pre>","").replace("</pre>","").replace("<br/>","\n")
-        return temp.strip()
+    def scrapeInputText(div):
+        ans = [i.text for i in div.find('pre').children]
+        return "\n".join(ans).strip()
 
-    inputDiv = soup.find_all('div',{"class":"input"})
-    outputDiv = soup.find_all('div',{"class":"output"})
+    def scrapeOutputText(div):
+        return div.find('pre').text.strip()
 
+    inputDiv = soup.find_all('div', {"class": "input"})
+    outputDiv = soup.find_all('div', {"class": "output"})
     for x in range(len(inputDiv)):
-        inputs.append(scrapeText(inputDiv[x]))
-        outputs.append(scrapeText(outputDiv[x]))
+        inputs.append(scrapeInputText(inputDiv[x]))
+        outputs.append(scrapeOutputText(outputDiv[x]))
     for ind in range(len(inputs)):
-        with open("test-"+str(ind+1)+".in","w") as f:
+        with open("test-"+str(ind+1)+".in", "w") as f:
             f.write(inputs[ind])
-        with open("test-"+str(ind+1)+".out","w") as f:
+        with open("test-"+str(ind+1)+".out", "w") as f:
             f.write(outputs[ind])
     os.chdir("..")
+
 
 try:
     dirName = args["url"].split('/')[-1]+"test"
@@ -57,36 +60,39 @@ else:
     os.chdir(dirName)
     getTests()
 
-tests = sorted([file.split('.')[0] for file in os.listdir(dirName) if 'in' in file])
+tests = sorted([file.split('.')[0]
+               for file in os.listdir(dirName) if 'in' in file])
 
 ext = args["file"][args["file"].find('.'):]
 
-def getCommand(ext,test):
-    if ext==".cpp":
+
+def getCommand(ext, test):
+    if ext == ".cpp":
         return "./a.out" + " < " + dirName + "/" + test + ".in > " + dirName + "/my" + test + ".out"
-    elif ext==".py":
+    elif ext == ".py":
         return "python3 " + args["file"] + " < " + dirName + "/" + test + ".in > " + dirName + "/my" + test + ".out"
 
+
 commands = ""
-if ext ==".cpp":
+if ext == ".cpp":
     command = "g++ " + args["file"] + " -std=c++17"
     print("Compiling source code...")
     os.system(command)
-elif ext==".py":
+elif ext == ".py":
     pass
 else:
     print("[Error]: Language not supported!")
     exit()
 
 for test in tests:
-    print("\nRunning",test,"=>",end=" ")
-    command = getCommand(ext,test)
+    print("\nRunning", test, "=>", end=" ")
+    command = getCommand(ext, test)
     op = os.system(command)
-    expectedFile = open(dirName+'/'+test+'.out',"r")
-    myresultFile = open(dirName+'/my'+test+'.out',"r")
+    expectedFile = open(dirName+'/'+test+'.out', "r")
+    myresultFile = open(dirName+'/my'+test+'.out', "r")
     expected = expectedFile.read().strip()
     myresult = myresultFile.read().strip()
-    if(expected==myresult):
+    if (expected == myresult):
         print("Passed!")
     else:
         print("Failed !!!")
